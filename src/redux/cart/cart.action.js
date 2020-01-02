4ae81c0588d6edb8fa77501ae415bc2e3e862fa1
Stefan <stefanrankovic011@yahoo.com>
2020-01-02 01:54:59 +0100

    updated shop data with new data, updated cary ultils to group items in cart reduce r

--- src/redux/cart/cart.action.js
+++ src/redux/cart/cart.action.js
@@ -1,5 +1,11 @@
 import CartActionTypes from './cart.types';
+import CarActionTypes from './cart.types';
 
 export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
+});
+
+export const addItem = item => ({
+    type: CarActionTypes.ADD_ITEM,
+    payload: item
 });
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,9 @@
 import CarActionTypes from './cart.types';
+import { addItemToCart } from './cart.utils'
 
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -10,10 +12,15 @@ switch(action.type) {
     return {
         ...state,
         hidden: !state.hidden
-    }
+    };
+    case CarActionTypes.ADD_ITEM:
+        return{
+            ...state,
+            cartItems: addItemToCart(state.cartItems, action.payload)
+        };
     default:
         return state;
 }
-}
+};
 
 export default cartReducer;
